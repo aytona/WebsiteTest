55504e59fc3c318800f3d20d174eb9fb7b938d44
aytona <christopher.aytona@gmail.com>
2016-07-16 04:28:32 -0400

    fontawesome, and social buttons added in navbar

--- js/main.js
+++ js/main.js
@@ -1,2 +1,1 @@
-function showCopyright() {var currentYear = new Date().getFullYear();var output = '<p><strong>Copyright © ' + currentYear + '</strong> Christopher Aytona</p>';document.getElementById("copyright").innerHTML = output;}
-$(document).ready(function(){$('[data-toggle="tooltip"]').tooltip();});
+function showCopyright() {var currentYear = new Date().getFullYear();var output = '<p><strong>Copyright © ' + currentYear + '</strong> Christopher Aytona</p>';document.getElementById("copyright").innerHTML = output;}$(document).ready(function(){$('[data-toggle="tooltip"]').tooltip();});
