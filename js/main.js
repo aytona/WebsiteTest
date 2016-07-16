d8a0315a961c03ffe80420466cb644e247ee2f05
aytona <christopher.aytona@gmail.com>
2016-07-16 00:13:41 -0400

    moved tooltip function to main.js to reduce redundancy, and made website buttons open on self

--- js/main.js
+++ js/main.js
@@ -1,1 +1,2 @@
 function showCopyright() {var currentYear = new Date().getFullYear();var output = '<p><strong>Copyright © ' + currentYear + '</strong> Christopher Aytona</p>';document.getElementById("copyright").innerHTML = output;}
+$(document).ready(function(){$('[data-toggle="tooltip"]').tooltip();});
