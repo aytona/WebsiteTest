0799210af8b8099c712258f1cbf533982decab81
Christopher Aytona <christopher.aytona@gmail.com>
2017-11-27 17:27:22 -0500

    Spaced out main to make it easier to read

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
-function showCopyright() {var currentYear = new Date().getFullYear();var output = '<p><strong>Copyright © ' + currentYear + '</strong> Christopher Aytona. All Rights Reserved.</p>';document.getElementById("copyright").innerHTML = output;}$(document).ready(function(){$('[data-toggle="tooltip"]').tooltip();});$(document).ready(function(){$(window).scroll(function(){if ($(this).scrollTop() > 30) {$('.scrollToTop').fadeIn();} else {$('.scrollToTop').fadeOut();}});$('.scrollToTop').click(function(){$('html, body').animate({scrollTop : 0},800);return false;});});
+// Updates the copyright date to the current date
+function showCopyright() {
+    var currentYear = new Date().getFullYear();
+    var output = '<p><strong>Copyright © ' + currentYear + '</strong> Christopher Aytona. All Rights Reserved.</p>';
+
+    document.getElementById("copyright").innerHTML = output;
+}
+
+// Scrolling to the top of the page function
+$(document).ready(function() {
+    $('[data-toggle="tooltip"]').tooltip();
+});
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 30) {
+            $('.scrollToTop').fadeIn();
+        } else {
+            $('.scrollToTop').fadeOut();
+        }
+    });
+    $('.scrollToTop').click(function() {
+        $('html, body').animate({ scrollTop: 0 }, 800);
+        return false;
+    });
+});
